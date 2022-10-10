3de8455ccb096a8289f709b539bda2dd4ef5a289
mujirahman <mujirahman888@gmail.com>
2022-10-10 15:03:14 +0700

    finising step

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 export const App = () => {
-  return(<p> Hello this is Template React typescript with webpack v.5</p>)
+  return(<h1>Template React typescript with webpack v.5 - {process.env.NODE_ENV} - {process.env.name}</h1>)
 }
